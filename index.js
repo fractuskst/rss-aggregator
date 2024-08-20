717798fcc0692cc31571b7e5f55454f3a13edc60
fractuskst <fractuskst@yandex.ru>
2024-08-20 21:25:46 +0300

    6 шаг. Предпросмотр постов

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ export default () => {
     addedUrls: [],
     posts: [],
     feeds: [],
-    uniquePostIds: new Set(),
+    visitedLinks: [],
+    modalId: '',
 
     form: {
       isValid: true,
+      status: 'filling', // filling, sending
       feedback: {
         errors: '',
         success: '',
@@ -42,28 +44,40 @@ export default () => {
   const validateUrl = (url) => {
     const schema = yup.string()
       .url(i18nInstance.t('errors.invalidURL'))
-      .notOneOf(state.addedUrls, i18nInstance.t('errors.alreadyExist'));
+      .notOneOf(state.addedUrls, i18nInstance.t('errors.alreadyExist'))
+      .required();
     return schema.validate(url);
   };
 
   const form = document.querySelector('.rss-form');
+  const input = document.querySelector('#url-input');
   const feedBackEl = document.querySelector('.feedback');
 
+  const updateFeedback = (type, message) => {
+    feedBackEl.textContent = message;
+    feedBackEl.classList.toggle('text-danger', type === 'error');
+    feedBackEl.classList.toggle('text-success', type === 'success');
+  };
+
   const watchedState = onChange(state, (path, currentValue) => {
+    if (path === 'form.status') {
+      const submitButton = document.querySelector('[type="submit"]');
+      if (currentValue === 'sending') {
+        submitButton.setAttribute('disabled', true);
+      } else {
+        submitButton.removeAttribute('disabled');
+      }
+    }
+
     if (path === 'form.feedback.success') {
-      feedBackEl.textContent = currentValue;
-      feedBackEl.classList.remove('text-danger');
-      feedBackEl.classList.add('text-success');
+      updateFeedback('success', currentValue);
     }
 
     if (path === 'form.feedback.errors') {
-      feedBackEl.textContent = currentValue;
-      feedBackEl.classList.remove('text-success');
-      feedBackEl.classList.add('text-danger');
+      updateFeedback('error', currentValue);
     }
 
     if (path === 'form.isValid') {
-      const input = document.querySelector('#url-input');
       input.classList.toggle('is-invalid', !currentValue);
     }
 
@@ -83,7 +97,6 @@ export default () => {
     }
 
     if (path === 'posts') {
-
       const postsDiv = document.querySelector('.posts');
       postsDiv.innerHTML = `<div class="card border-0">
       <div class="card-body"><h2 class="card-title h4">Посты</h2></div>
@@ -97,39 +110,47 @@ export default () => {
         list.appendChild(li);
       });
     }
+
+    if (path === 'modalId') {
+      const currentPost = watchedState.posts.find((post) => post.id === currentValue);
+      const modalTitle = document.querySelector('.modal-title');
+      const modalBody = document.querySelector('.modal-body');
+      const modalLink = document.querySelector('a.full-article');
+      modalTitle.textContent = currentPost.title;
+      modalBody.textContent = currentPost.description;
+      modalLink.setAttribute('href', currentPost.link);
+    }
+
+    if (path === 'visitedLinks') {
+      const id = _.last(currentValue);
+      const link = document.querySelector(`a[data-id="${id}"]`);
+      link.classList.replace('fw-bold', 'fw-normal');
+      link.classList.add('link-secondary');
+    }
   });
 
   const checkForNewPosts = () => {
-    console.log('Checking for new posts...');
     const promises = state.addedUrls.map((url) => 
       axios.get(`${routes.allOrigins()}${url}`, { timeout: 10000 })
         .then((response) => {
           const parsedData = parseDOM(response.data.contents);
           const posts = parsedData.querySelectorAll('item');
-          
+          const newPostsArray = [];
           posts.forEach((post) => {
             const postTitle = post.querySelector('title').textContent;
             const postDescription = post.querySelector('description').textContent;
             const postLink = post.querySelector('link').textContent;
             const postId = post.querySelector('guid') ? post.querySelector('guid').textContent : postLink;
-
-            if (!state.uniquePostIds.has(postId)) {
-              watchedState.posts.push({
-                id: _.uniqueId(),
-                title: postTitle,
-                description: postDescription,
-                link: postLink,
-              });
-              state.uniquePostIds.add(postId);
-              console.log('New post added:', postTitle);
+            if (!_.some(state.posts, ['id', postId])) {
+              newPostsArray.push({ id: postId, title: postTitle, description: postDescription, link: postLink });
             }
           });
+          watchedState.posts.unshift(...newPostsArray);
         })
         .catch((err) => {
           console.error(err);
         })
       );
-
       Promise.all(promises).finally(() => {
         setTimeout(checkForNewPosts, 5000);
       });
@@ -145,7 +166,7 @@ export default () => {
         watchedState.form.feedback.errors = '';
         watchedState.form.feedback.success = '';
         watchedState.form.isValid = true;
-        
+        watchedState.form.status = 'sending';
         return axios.get(`${routes.allOrigins()}${validUrl}`, { timeout: 10000 });
       })
       .then((response) => {
@@ -155,29 +176,36 @@ export default () => {
             watchedState.form.feedback.errors = i18nInstance.t('errors.notContainRSS');
           } else {
             watchedState.addedUrls.push(url);
-            watchedState.form.feedback.success = i18nInstance.t('success');
+            watchedState.form.feedback.success = i18nInstance.t('success')
           }
 
         const feedtitle = parsedData.querySelector('title');
         const feedDescription = parsedData.querySelector('description');
-        watchedState.feeds.push({
+        watchedState.feeds.unshift({
           title: feedtitle.textContent, 
           description: feedDescription.textContent,
         });
 
         const posts = parsedData.querySelectorAll('item');
+        const postsArray = [];
         posts.forEach((post) => {
           const postTitle = post.querySelector('title').textContent;
           const postDescription = post.querySelector('description').textContent;
           const postLink = post.querySelector('link').textContent;
-          watchedState.posts.push({
-            id: _.uniqueId(),
+          const postId = post.querySelector('guid') ? post.querySelector('guid').textContent : postLink;
+          postsArray.unshift({
+            id: postId,
             title: postTitle,
             description: postDescription,
             link: postLink,
           })
         });
-        setTimeout(checkForNewPosts, 5000);
+        postsArray.reverse();
+        watchedState.posts.unshift(...postsArray);
+        e.target.reset();
+        input.focus();
+        watchedState.form.status = 'filling';
+        checkForNewPosts();
       })
       .catch((err) => {
         watchedState.form.isValid = false;
@@ -188,6 +216,22 @@ export default () => {
         } else {
           watchedState.form.feedback.errors = err.message;
         }
+        watchedState.form.status = 'filling';
       });
   });
+
+  const modal = document.getElementById('modal');
+  modal.addEventListener('show.bs.modal', (e) => {
+    const currentPostId = e.relatedTarget.getAttribute('data-id');
+    watchedState.visitedLinks.push(currentPostId);
+    watchedState.modalId = currentPostId;
+  });
+
+  const links = document.querySelectorAll('.fw-bold');
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      const linkId = link.getAttribute('data-id');
+      watchedState.visitedLinks.push(linkId);
+    });
+  })
 };
--- locales/ru.js
+++ locales/ru.js
@@ -4,9 +4,8 @@ export default {
       invalidURL: 'Ссылка должна быть валидным URL',
       notContainRSS: 'Ресурс не содержит валидный RSS',
       alreadyExist: 'RSS уже существует',
-      serverError: 'Server error: {{status}}',
-      networkError: 'Network Problems. Try again',
-      unknown: 'Unknown error. Try again',
+      networkError: 'Ошибка сети',
+      required: 'Не должно быть пустым',
     },
 
     success: 'RSS успешно загружен',
